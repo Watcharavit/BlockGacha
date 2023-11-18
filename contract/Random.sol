96868b265bfb58813a99ecbe4cfe90ffddf7b234
fah <natapat.main@gmail.com>
2023-11-19 05:58:38 +0700

    force drop when actual drop rate gone too low

--- contract/Random.sol
+++ contract/Random.sol
@@ -9,16 +9,19 @@ contract GachaGame is ItemTrading {
         bytes32 itemID;
         string name;
         uint rate;  // Drop rate per million
+        uint actualDrops; // Actual number of times the item has been dropped
     }
 
     GachaItem[] public gachaItems;
     uint public totalRate; // Total rate of all added items
+    uint public totalPulls; // Total number of gacha pulls
     
     address public owner;
 
     constructor() {
         owner = msg.sender;
         totalRate = 0;
+        totalPulls = 0;
         
         // Predefined gacha items added in the constructor
         _addGachaItem("itemID1", "Sword of Destiny", 300000);
@@ -26,28 +29,64 @@ contract GachaGame is ItemTrading {
         _addGachaItem("itemID3", "Healing Potion", 200000);
     }
 
-    // Internal function to add new gacha items
     function _addGachaItem(bytes32 _itemID, string memory _name, uint _rate) internal {
-        gachaItems.push(GachaItem(_itemID, _name, _rate));
+        gachaItems.push(GachaItem(_itemID, _name, _rate, 0));
         totalRate += _rate;
     }
 
-    // Simple random number generator
     function random() internal view returns (uint) {
         return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % totalRate;
     }
 
-    // Gacha pull function
     function gachaPull() public {
+        totalPulls++;
         uint randomNumber = random();
         uint cumulativeRate = 0;
+
+        // Check if any item's drop rate is significantly low
+        bytes32 forcedItemID = checkForcedDrop();
+        if (forcedItemID != bytes32(0)) {
+            forceDropItem(forcedItemID);
+            return;
+        }
 
         for (uint i = 0; i < gachaItems.length; i++) {
             cumulativeRate += gachaItems[i].rate;
             if (randomNumber < cumulativeRate) {
-                addItem(gachaItems[i].itemID, gachaItems[i].name, gachaItems[i].rate);
+                dropItem(i);
                 break;
             }
         }
+    }
+
+    function checkForcedDrop() internal view returns (bytes32) {
+        for (uint i = 0; i < gachaItems.length; i++) {
+            GachaItem storage item = gachaItems[i];
+            if (isDropRateTooLow(item)) {
+                return item.itemID;
+            }
+        }
+        return bytes32(0);
+    }
+
+    function isDropRateTooLow(GachaItem storage item) internal view returns (bool) {
+        uint expectedDrops = (totalPulls * item.rate) / totalRate;
+        // Apply simple hypothesis test with a rejecting region of 5%
+        return item.actualDrops < (expectedDrops * 95 / 100);
+    }
+
+    function forceDropItem(bytes32 itemID) internal {
+        for (uint i = 0; i < gachaItems.length; i++) {
+            if (gachaItems[i].itemID == itemID) {
+                dropItem(i);
+                return;
+            }
+        }
+    }
+
+    function dropItem(uint index) internal {
+        GachaItem storage item = gachaItems[index];
+        item.actualDrops++;
+        addItem(item.itemID, item.name, item.rate);
     }
 }
