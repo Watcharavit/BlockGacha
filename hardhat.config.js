ec06adcdb552a2772e90aa3eac615395a6c72f40
watcharavit <watcha.ea@hotmail.com>
2023-12-03 12:48:29 +0700

    Setup solidity compiler

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomiclabs/hardhat-ethers");
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+	solidity: "0.8.15",
+	defaultNetwork: "sepolia",
+	networks: {
+		hardhat: {},
+		sepolia: {
+			url: process.env.API_URL,
+			accounts: [`0x${process.env.PRIVATE_KEY}`],
+			gas: 210000000,
+			gasPrice: 800000000000
+		}
+	}
+};
