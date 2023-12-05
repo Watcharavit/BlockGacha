c68c51b41d904482ca2456dab7b77f40c9e4758d
watcharavit <watcha.ea@hotmail.com>
2023-12-05 18:54:43 +0700

    Making hardhat.config for compile only

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,19 +3,20 @@
  */
 
 require("@nomiclabs/hardhat-ethers");
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 module.exports = {
-	solidity: "0.8.15",
-	defaultNetwork: "sepolia",
-	networks: {
-		hardhat: {},
-		sepolia: {
-			url: process.env.API_URL,
-			accounts: [`0x${process.env.PRIVATE_KEY}`],
-			gas: 210000000,
-			gasPrice: 800000000000
-		}
-	}
+	solidity: "0.8.15"
+	// USE THIS IF YOU PLAN TO RUN npx hardhat run <script>
+	// defaultNetwork: "sepolia",
+	// networks: {
+	// 	hardhat: {},
+	// 	sepolia: {
+	// 		url: process.env.API_URL,
+	// 		accounts: [`0x${process.env.PRIVATE_KEY}`],
+	// 		gas: 210000000,
+	// 		gasPrice: 800000000000
+	// 	}
+	// }
 };
